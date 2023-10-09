154ffc07be26f2235a1a854e050e89b58789ef18
Ashik-Rahman100 <ashik5515@gmail.com>
2023-10-10 01:07:00 +0600

    [done] update method with department

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import type { TypedUseSelectorHook } from "react-redux";
 import { useDispatch, useSelector } from "react-redux";
 import type { AppDispatch, RootState } from "./store";
@@ -5,3 +6,22 @@ import type { AppDispatch, RootState } from "./store";
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+// Handle debounce department search term
+interface IDebounce {
+  searchQuery: string;
+  delay: number;
+}
+export const useDebounced = ({ searchQuery, delay }: IDebounce) => {
+  const [debouncedValue, setDebouncedValue] = useState<string>(searchQuery);
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(searchQuery);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [searchQuery, delay]);
+  return debouncedValue;
+};
